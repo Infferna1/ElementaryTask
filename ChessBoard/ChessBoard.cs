9a46ad7733f80fa6e4584588cb82757743c6ec12
Nikita Semenov <semenovnikita2013@gmail.com>
2021-05-28 19:52:23 +0300

    Добавьте файлы проекта.

---
+++ ChessBoard/ChessBoard.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessBoard
+{
+    class ChessBoard
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        
+        public ChessBoard (/*int height, int width*/)
+        {
+            //Height = height;
+            //Width = width;
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Enter height:");
+            Height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter width:");
+            Width = int.Parse(Console.ReadLine());
+        }
+
+        public void Output()
+        {
+            for (int i = 0; i < Height; i++, Console.WriteLine())
+                for (int j = 0; j < Width; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write(" ");
+
+                    }
+                    else
+                    {
+                        Console.Write("*");
+
+                    }
+                }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ChessBoard/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChessBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChessBoard chessBoard = new ChessBoard();
+            chessBoard.Input();
+            chessBoard.Output();
+        }
+    }
+}
